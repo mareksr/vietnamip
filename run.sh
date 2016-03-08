dc734ebb7058c8d22ebf499f255807d502d3818e
Marek <mareksr@hm.pl>
2016-03-08 21:20:05 +0100

    ok

--- run.sh
+++ run.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 
-#./2vietnam.sh > l.txt
+./2vietnam.sh > l.txt
 ./3vietnam.pl > black.ip.10
+
 git commit -m "ok"
 git push
 
+#load ips
+./loadIP.sh
+
+#load nets
 ./3load.sh
+
 
 
 
