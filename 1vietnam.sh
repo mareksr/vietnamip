9710e99ee2fc7f2560390d25b18201029a981fb0
Marek <mareksr@hm.pl>
2016-07-20 08:17:05 +0200

    ol

--- 1vietnam.sh
+++ 1vietnam.sh
@@ -4,9 +4,9 @@
 
 #ostatnie 30 dni blacklogow
 
-find /usr/local/assp/logs/ -mtime -31 -name *bmail* | while read line
+find /usr/local/assp/logs/ -mtime -31 -name *bmail*gz | while read line
 do
-	cat $line | grep -E -o "(25[0-5]|2[0-4][0-9]|[1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[1]?[0-9][0-9]?) <\b[a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9.-]+\b>" >> /tmp/miki.txt
+	zcat $line | grep -E -o "(25[0-5]|2[0-4][0-9]|[1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[1]?[0-9][0-9]?) <\b[a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9.-]+\b>" >> /tmp/miki.txt
 
 done
 
