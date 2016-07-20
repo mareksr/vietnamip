f28d62ea84f652eca864f236ccdc08193d60b5df
Marek <mareksr@hm.pl>
2016-07-20 09:09:47 +0200

    ok

--- 1vietnam.sh
+++ 1vietnam.sh
@@ -6,7 +6,17 @@
 
 find /usr/local/assp/logs/ -mtime -31 -name *bmail*gz | while read line
 do
-	zcat $line | grep -E -o "(25[0-5]|2[0-4][0-9]|[1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[1]?[0-9][0-9]?) <\b[a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9.-]+\b>" >> /tmp/miki.txt
+	echo $line
+	zcat $line | grep -E -o "(25[0-5]|2[0-4][0-9]|[1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[1]?[0-9][0-9]?) <\b[a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9.-]+\b>" >> /tmp/.miki.txt
 
 done
+
+
+#get if #ip is >10
+
+cat /tmp/.miki.txt  | cut -d ' ' -f 1 | sort | uniq -c | sort -n | awk -v limit=10 '$1 > limit{print $2}' > /tmp/miki.txt
+
+
+
+
 
