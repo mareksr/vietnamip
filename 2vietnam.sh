d079ea2dedc1718d510b6cd32f6b7f6bdb69186d
Marek <mareksr@hm.pl>
2016-07-20 09:56:00 +0200

    ok

--- 2vietnam.sh
+++ 2vietnam.sh
@@ -2,7 +2,7 @@
 
 
 cat /tmp/miki.txt  | cut -d ' ' -f 1 > /tmp/miki2.txt
-combine /tmp/miki2.txt not whitelist.out  > /tmp/miki3.txt
+combine /tmp/miki2.txt not /opt/megaspam/vietnamip/whitelist.out  > /tmp/miki3.txt
 
 cat /tmp/miki3.txt | sort | uniq -c | sort | while read line
 do
@@ -12,5 +12,4 @@ do
 done
 
 
-#Mar-09-16 00:00:33 m1-78030-02340 [Worker_1] [URIBL] 74.221.210.19 <DiabetesDefeatedSystem@uscrez.win> to: informatyk@e-fresh.pl [spam found] (URIBLcache: fail, uscrez.win listed in multi.surbl.org) [Why This Secret Ingredient Reverses Type 2 Diabetes] -> /usr/local/assp5/spam/2340.eml;
 
