ad1266d28793ccac9edd45594422e730180c5b69
Marek <mareksr@hm.pl>
2016-08-29 11:25:33 +0200

    autocommit on change

--- 1vietnam.sh
+++ 1vietnam.sh
@@ -5,7 +5,7 @@
 
 #ostatnie 30 dni blacklogow
 
-find /usr/local/assp/logs/ -mtime -31 -name *bmail*gz | while read line
+find /shared/ -mtime -31 -name *bmail*gz  | while read line
 do
 	zcat $line | grep -v passing | grep -E -o "(25[0-5]|2[0-4][0-9]|[1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[1]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[1]?[0-9][0-9]?) <\b[a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9.-]+\b>" >> /tmp/.miki.txt
 
